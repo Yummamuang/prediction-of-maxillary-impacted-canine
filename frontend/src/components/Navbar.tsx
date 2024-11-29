eb5e9775854b658f2604d346da688668b0e79354
Yummamuang <oak.ptkn@gmail.com>
2024-11-29 10:58:22 +0700

    -

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,16 @@
+//Logo
+import eduSign from '../../public/logo/edusign-logo.svg'
+
 function Navbar() {
     
     return (
         <div>
-            
+            <div className="logo">
+                <div style={{ width: '50px' , height: '50px'}}>
+                    <img src={eduSign} alt="logo" />
+                </div>
+                
+            </div>
         </div>
     )
 }
