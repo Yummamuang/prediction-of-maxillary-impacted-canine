b50da0934847650fed258118f00f0808ca8a6d61
Yummamuang <oak.ptkn@gmail.com>
2024-11-29 11:24:45 +0700

    -

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,16 @@
-//Logo
-import eduSign from '../../public/logo/edusign-logo.svg'
+//Import Logo
+import eduSign from '/logo/edusign-logo.svg'
 
 function Navbar() {
     
     return (
-        <div>
-            <div className="logo">
-                <div style={{ width: '50px' , height: '50px'}}>
+        <div id='navbar' className='bg-white' >
+            {/* logo + text */}
+            <div className="flex items-center logo">
+                <div style={{ width: '2rem' , height: '2rem'}}>
                     <img src={eduSign} alt="logo" />
                 </div>
-                
+                <div className='text-xl atma-semibold'>EduSign</div>
             </div>
         </div>
     )
